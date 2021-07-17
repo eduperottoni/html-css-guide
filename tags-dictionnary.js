7a9772c7e613677675f41f994aa794799ba78f3e
eduperottoni <edu.perottoni@gmail.com>
2021-07-17 02:19:16 -0300

    Implementando a tradução nas três opções de clique que o usuário possui para alterar o idioma da página

---
+++ tags-dictionnary.js
@@ -0,0 +1,54 @@
+const lanButton = document.querySelector('#lan-but')
+const lanButtonDot = document.querySelector('#lan-but #lan-but-dot')
+const lanFlagUsa = document.getElementById('usa-flag')
+const lanFlagBra = document.getElementById('bra-flag')
+const textPt = document.querySelectorAll('.pt')
+const textEn = document.querySelectorAll('.en')
+
+var isEn = true //Variável global que determina a linguagem da SPA
+hideLanguage(textPt)
+
+function hideLanguage(langNodeList){
+    Array.from(langNodeList).map((item) => {
+        item.style.display = 'none';
+    })
+}
+
+function showLanguage (langNodeList){
+    Array.from(langNodeList).map((item) =>{
+        item.style.display = 'initial'
+    })
+}
+
+const changeLanguage = () =>{
+    isEn ? 
+    (hideLanguage(textPt), showLanguage(textEn)):
+    (hideLanguage(textEn), showLanguage(textPt))
+}
+//Quando o usuário clicar na bandeira do Brasil
+lanFlagBra.addEventListener('click', (e) =>{
+    lanButtonDot.className = 'is-pt'
+    isEn = false
+    changeLanguage()
+})
+//Quando o usuário clicar na bandeira dos Estados Unidos
+lanFlagUsa.addEventListener('click', (e) =>{
+    lanButtonDot.className = 'is-en'
+    isEn = true
+    changeLanguage()
+})
+//Quando o uauário clicar no botão central
+lanButton.addEventListener('click', (e) =>{
+    e.preventDefault()
+
+    if(lanButtonDot.className == 'is-en'){
+        lanButtonDot.className = 'is-pt'
+        isEn = false
+    }else{
+        lanButtonDot.className = 'is-en'
+        isEn = true
+    }
+       
+    changeLanguage()
+})
+
